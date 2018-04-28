950f2973a72bc9f342abe06a7b2e7a922f3ac013
piiy <casv2685@student.su.se>
2018-04-28 10:04:20 +0200

    no real changes, just testing

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 /* Change these to change the pages in the tabcontroller*/
 import { AboutPage } from '../about/about';
 import { HomePage } from '../home/home';
+import { VenuePage } from '../tabs/tabs';
 
 @Component({
   templateUrl: 'tabs.html'
