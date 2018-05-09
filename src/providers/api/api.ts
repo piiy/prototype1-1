9a41afa59648485699e4a8accaaa7ed1fd34ad59
Joonteee <34379335+Joonteee@users.noreply.github.com>
2018-05-09 16:05:33 +0200

    Update api.ts

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -18,7 +18,7 @@ export class ApiProvider {
       let headers = new HttpHeaders();
     let other_headers = headers.append('Authorization', 'Bearer ' + this.jwt.makeToken(str));
       console.log("headers: ",JSON.stringify(headers));
-    var response = this.http.get("https://pvt.dsv.su.se/Group9/jwtSharp", {headers: other_headers});
+    var response = this.http.get("https://pvt.dsv.su.se/Group9/venues");
       return response;
 }
 getEvents(str:string) {
