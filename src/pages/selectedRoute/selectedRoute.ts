0080b018b6c6cf203b6191b8ebd3ba39c56b7bd3
Joonteee <34379335+Joonteee@users.noreply.github.com>
2018-05-18 13:39:42 +0200

    Update selectedRoute.ts
    Fixat så att den byter färg. Detta kan hända att vi gör lite snyggare (rent kodmessigt) när API:t har en metod för det.

--- src/pages/selectedRoute/selectedRoute.ts
+++ src/pages/selectedRoute/selectedRoute.ts
@@ -10,41 +10,47 @@ import { TravelInfo2 } from '../travelInfo2/travelInfo2';
 })
 
 export class SelectedRoute {
-
+  private venueId;
+  public transport_type;
   departures;
   public routeName;
   public siteId;
-  public transport_type;
-  
+  public aColor: string;
+public bColor: string;
+stationInformation;
+
   constructor(public navCtrl: NavController, public provider: ApiProvider, public popoverCtrl: PopoverController, public navParams: NavParams) {
     this.routeName = navParams.get("routeName");
     this.siteId = navParams.get("siteId");
-    this.transport_type = navParams.get("transport_type");
+   this.transport_type = navParams.get("transport_type");
+  this.venueId = navParams.get("venueId");
+
     this.getDepartures(this.siteId);
-  
+    this.getStationInformation(this.venueId);
+
   }
-  
+
   getDepartures(siteId:string) {
 this.provider.getDepartures(siteId)
 .subscribe(
-  (data) => {
+ (data) => {
 
 if(this.transport_type == '1') {
-  this.departures = data["ResponseData"].Metros;
+ this.departures = data["ResponseData"].Metros;
 } else if(this.transport_type == '2') {
-  this.departures = data["ResponseData"].Trams;
+ this.departures = data["ResponseData"].Trams;
 } else if(this.transport_type == '3') {
-  this.departures = data["ResponseData"].Buses;
+ this.departures = data["ResponseData"].Buses;
 }
 
-  },
-  (error) => {console.log("Error: ", JSON.stringify(error));}
+ },
+ (error) => {console.log("Error: ", JSON.stringify(error));}
 )
-  }
+ }
 
- 
+
   openDisturbanceInfo(myEvent) { // Skapar popup-sida med störningsinfo.
-    
+
     let popover = this.popoverCtrl.create(DisturbanceInfo, {siteId: this.siteId});
     popover.present({
       ev: myEvent
@@ -58,4 +64,26 @@ if(this.transport_type == '1') {
     ev: myEvent
   });
 }
+getStationInformation(venue:string) { // Kommer att hÃ¤mta olika stationer frÃ¥n API
+    this.provider.getStations(venue)
+    .subscribe(
+      (data)=> {
+        this.stationInformation=data["results"];
+this.changeColors();
+      },
+      (error)=> {console.log("error: ", JSON.stringify(error));}
+    )
+  }
+  changeColors(){
+    var size = Object.keys(this.stationInformation).length;
+    for(let i = 0; i < size; i++){
+      console.log("Transport_type: ", this.stationInformation[i].transport_type);
+        if(this.stationInformation[i].e_name==this.routeName&&this.stationInformation[i].transport_type==this.transport_type){
+          console.log("Ja!");
+          this.aColor = this.stationInformation[i].color_hex;
+          this.bColor = this.stationInformation[i].color_hex;
+        }
+    }
+  }
+
 }
