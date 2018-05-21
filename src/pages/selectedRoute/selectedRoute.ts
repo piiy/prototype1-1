889b78234bceeab494937ba9cac173278ec7b28e
Joonteee <34379335+Joonteee@users.noreply.github.com>
2018-05-21 12:16:51 +0200

    Update selectedRoute.ts

--- src/pages/selectedRoute/selectedRoute.ts
+++ src/pages/selectedRoute/selectedRoute.ts
@@ -94,23 +94,6 @@ export class SelectedRoute {
   getDepartures(siteId:string) {
     console.log("Uppdaterar");
     this.provider.getDepartures(siteId)
-    .subscribe(
-      (data) => {
-
-        if(this.transport_type == '1') {
-          this.departures = data["ResponseData"].Metros;
-        } else if(this.transport_type == '2') {
-          this.departures = data["ResponseData"].Trams;
-        } else if(this.transport_type == '3') {
-          this.departures = data["ResponseData"].Buses;
-        }
-
-      },
-      (error) => {console.log("Error: ", JSON.stringify(error));}
-    )
-  }
-
-this.provider.getDepartures(siteId)
 .subscribe(
  (data) => {
 
