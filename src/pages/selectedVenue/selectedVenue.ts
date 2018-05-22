4c7ddd082a14715a42811cc411efc0d1496155bf
Andreas Pejle <andpej@gmail.com>
2018-05-22 11:34:26 +0200

    Gubbarna visas nu random i olika färger

--- src/pages/selectedVenue/selectedVenue.ts
+++ src/pages/selectedVenue/selectedVenue.ts
@@ -3,6 +3,7 @@ import { NavController, ViewController, PopoverController, NavParams } from 'ion
 import { TravelInfo } from '../travelInfo/travelInfo';
 import { ApiProvider } from '../../providers/api/api';
 import { SelectedRoute } from '../selectedRoute/selectedRoute';
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser';
 
 
 @Component({
@@ -14,10 +15,15 @@ export class SelectedVenue {
   public venueName;
   public venueId;
 
-  constructor(public navCtrl: NavController, public popoverCtrl: PopoverController, public provider: ApiProvider, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public popoverCtrl: PopoverController, public provider: ApiProvider, public navParams: NavParams, public inAppBrowser: InAppBrowser) {
   this.venueName = navParams.get("venueName");
   this.venueId = navParams.get("venueId");
   this.ionLoadStations(this.venueId);
+  }
+
+  openInAppBrowser() {
+    
+    const browser = this.inAppBrowser.create('https://www.stockholmlive.com/evenemang/alla-evenemang', '_self')
   }
 
  goToselectedRoute(routeName:string, siteId:string, tType, icon){
